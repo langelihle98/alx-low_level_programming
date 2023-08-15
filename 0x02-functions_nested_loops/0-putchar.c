ca6db371ba78ebda53d7e43e656adafe798a045e
Langelihle Mabaleka <mabalekalangelihle598@gmail.com>
2023-08-15 14:07:28 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
-#include "main.h"
+#include <unistd.h>
 
-int main()
-{printPutchar
-return 0;
+int _putchar(char c) {
+    return write(1, &c, 1);
 }
 
-void printPutchar() {
-    printf("_putchar\n");
+int main() {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return 0;
 }
+
 
