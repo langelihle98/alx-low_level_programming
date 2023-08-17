194313cb845bc622db79d22d35264573a3c1a1bd
Langelihle Mabaleka <mabalekalangelihle598@gmail.com>
2023-08-17 08:38:30 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,13 +9,13 @@
 
 void print_numbers(void)
 {
-        char i;
+char i;
 
-        i = 0;
-        while (i < 10)
-        {
-                _putchar(i + '0');
-                i++;
-        }
-        _putchar('\n');
+i = 0;
+while (i < 10)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
 }
