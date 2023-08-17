988e84d141cb23671314725b3ab73f7feee4c7ae
Langelihle Mabaleka <mabalekalangelihle598@gmail.com>
2023-08-17 12:14:13 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+int _putchar(char c);
+
+/**
+ * more_numbers - Prints 10x the numbers from 0 to 14, followed by new line
+ */
+void more_numbers(void)
+{
+int i, j;
+
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i >= 10)
+{
+_putchar('0' + (i / 10));
+}
+_putchar('0' + (i % 10));
+}
+_putchar('\n');
+}
+}
+
