37f65b26ab38d76af35e3304182e889c6631c0f0
Langelihle Mabaleka <mabalekalangelihle598@gmail.com>
2023-08-17 15:12:39 -0700

    8-print_suare.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+int _putchar(char c);
+
+/**
+ * print_square - Prints a square of '#' characters.
+ * @size: The size of the square.
+ */
+void print_square(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
+
