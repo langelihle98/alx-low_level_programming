ffb78885225ee379e1210206f686bad919c563fd
Langelihle Mabaleka <mabalekalangelihle598@gmail.com>
2023-08-17 11:32:04 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,18 +8,18 @@
 
 void print_most_numbers(void)
 {
-        int num;
+int num;
 
-        for (num = 0; num <= 9; num++)
-        {
-                if (num == 2 || num == 4)
-                {
-                        continue;
-                }
-                else
-                {
-                        _putchar(num + '0');
-                }
-        }
-        _putchar('\n');
+for (num = 0; num <= 9; num++)
+{
+if (num == 2 || num == 4)
+{
+continue;
+}
+ else
+{
+_putchar(num + '0');
+}
+}
+_putchar('\n');
 }
