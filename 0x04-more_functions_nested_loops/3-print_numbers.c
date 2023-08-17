10c47b3115d73ed29ade500b5c53531dccd37b0e
Langelihle Mabaleka <mabalekalangelihle598@gmail.com>
2023-08-17 07:42:52 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output.
+ * @c: The character to be written.
+ *
+ * Return: On success, 1. On error, -1 is returned,
+ *         and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ */
+void print_numbers(void)
+{
+char number;
+
+for (number = '0'; number <= '9'; number++)
+_putchar(number);
+
+_putchar('\n');
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function serves as the entry point for the program. It
+ * calls the print_numbers function to print the numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+print_numbers();
+return (0);
+}
+
