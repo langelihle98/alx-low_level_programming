cd770b9a11b2ac082f84c39117558632ebb9ad65
Langelihle Mabaleka <mabalekalangelihle598@gmail.com>
2023-08-18 07:26:42 -0700

    update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,11 @@ int main(void)
 
 printf("Infinite loop incoming :(\n");
 
-/*i = 0;*/
-/* while (i < 10)*/
-/* {*/
-/* putchar(i);*/
-/* }*/
+/* i = 0; */
+/* while (i < 10) */
+/* { */
+/* putchar(i); */
+/* } */
 printf("Infinite loop avoided! \\o/\n");
 /*
 * The "\\o/" is used to show the successful resolution of a situation.
