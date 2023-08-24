e110357b6831c151dd626d3b2a4eba0b55b29827
Langelihle Mabaleka <mabalekalangelihle598@gmail.com>
2023-08-24 03:40:31 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * *_strcat - concatenates two strings
+ *
+ * @dest: first string
+ *
+ * @src: second string
+ *
+ * @n: the number of bytes from src to be appended on dest
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0;src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
+
+}
